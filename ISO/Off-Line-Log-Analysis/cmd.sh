585339cb659bb7ac5fc3021f8f346caccd7b0e88
He Xiao <hexiao2@illinois.edu>
2014-11-19 22:50:57 -0600

    modify insert.log

--- ISO/Off-Line-Log-Analysis/cmd.sh
+++ ISO/Off-Line-Log-Analysis/cmd.sh
@@ -1,1 +1,1 @@
-existingApp/monpoly-1.1.2/monpoly -sig policy4_ldcc/insert/insert.sig -formula policy4_ldcc/insert/insert.mfotl -log ldcc.csv -negate
+existingApp/monpoly-1.1.2/monpoly -sig policy4_ldcc/insert/insert.sig -formula policy4_ldcc/insert/insert.mfotl -log policy4_ldcc/insert/insert.log -negate

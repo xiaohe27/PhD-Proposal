806fe46e475da71eb0346a0c68b3038cff2b0051
HE XIAO <hexiao2@illinois.edu>
2014-12-05 16:31:20 -0600

    add experiment result file

--- ISO/Off-Line-Log-Analysis/RE-PRODUCE/RVM/hugeLog/propWithEQ/bigLog.sh
+++ ISO/Off-Line-Log-Analysis/RE-PRODUCE/RVM/hugeLog/propWithEQ/bigLog.sh
@@ -4,6 +4,6 @@ echo "Start time : $Start" > time.txt
 ./csv2log 4 ldcc.csv | existingApp/rv13-experiments/monpoly/monpoly -sig sig -formula insert.mfotl -negate > insert2HugeLog.txt
 
 End=$(date +"%s")
-echo "Finish time : $End"
+echo "Finish time : $End" >> time.txt
 Diff=$(( $End - $Start ))
 echo "\nIt took $Diff seconds to analyze the insert property of the huge log ldcc.csv" >> time.txt

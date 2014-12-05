2897cae67075c8e0ff84b1dfce5d9fdee8046ffb
HE XIAO <hexiao2@illinois.edu>
2014-12-04 21:09:03 -0600

    pub.mfotl modified

--- ISO/Off-Line-Log-Analysis/existingApp/monpoly-1.1.2/run.sh
+++ ISO/Off-Line-Log-Analysis/existingApp/monpoly-1.1.2/run.sh
@@ -1,3 +1,3 @@
-#./monpoly -sig examples/rv11.sig -formula examples/rv11.mfotl -log examples/rv11.log -negate
+./monpoly -sig examples/rv11b.sig -formula examples/rv11b.mfotl -log examples/rv11b.log -negate
 
-./monpoly -sig examples/ex.sig -formula examples/ex.mfotl -log examples/ex.log -negate
+#./monpoly -sig examples/ex.sig -formula examples/ex.mfotl -log examples/ex.log -negate

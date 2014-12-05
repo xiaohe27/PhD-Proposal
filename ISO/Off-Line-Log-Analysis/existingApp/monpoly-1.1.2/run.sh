1f8cabc5ef94c680ad1412f9e9fd62d5bdf60002
HE XIAO <hexiao2@illinois.edu>
2014-12-04 20:21:33 -0600

    add myToy new eg!

---
+++ ISO/Off-Line-Log-Analysis/existingApp/monpoly-1.1.2/run.sh
@@ -0,0 +1,3 @@
+#./monpoly -sig examples/rv11.sig -formula examples/rv11.mfotl -log examples/rv11.log -negate
+
+./monpoly -sig examples/ex.sig -formula examples/ex.mfotl -log examples/ex.log -negate
---
+++ ISO/Off-Line-Log-Analysis/myToy/cmd.sh
@@ -0,0 +1,1 @@
+../existingApp/monpoly-1.1.2/monpoly -sig Pub.sig -formula Pub.mfotl -log Pub.log -negate

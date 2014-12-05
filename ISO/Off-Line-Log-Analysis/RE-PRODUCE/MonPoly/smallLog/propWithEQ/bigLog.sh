6058c760e19e57bb623c7ed702aaeb89f4565442
HE XIAO <hexiao2@illinois.edu>
2014-12-05 15:48:03 -0600

    add structure for doing reproduce

---
+++ ISO/Off-Line-Log-Analysis/RE-PRODUCE/MonPoly/smallLog/propWithEQ/bigLog.sh
@@ -0,0 +1,9 @@
+Start=$(date +"%s")
+echo "Start time : $Start" > time.txt
+
+./csv2log 4 ldcc.csv | existingApp/rv13-experiments/monpoly/monpoly -sig sig -formula insert.mfotl -negate > insert2HugeLog.txt
+
+End=$(date +"%s")
+echo "Finish time : $End"
+Diff=$(( $End - $Start ))
+echo "\nIt took $Diff seconds to analyze the insert property of the huge log ldcc.csv" >> time.txt
